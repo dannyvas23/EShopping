19886687bbafa9e4dd6149baaa9106db5b4bcae5
Danny Vasquez <dannyvas15@gmail.com>
2024-03-24 18:29:41 -0500

    Creando Microservicio Basket

---
+++ Services/Basket/Basket.Core/Entities/ShoppingCarItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Core.Entities;
+
+public class ShoppingCarItem
+{
+    public int  Quantity { get; set; }
+    public decimal Price { get; set; }
+    public int ProductId { get; set; }
+    public string ImageFile { get; set; }
+    public string ProductName { get; set; }
+    
+    
+}
